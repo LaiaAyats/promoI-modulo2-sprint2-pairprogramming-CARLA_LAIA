d7fd40af32c3aa782f4e90c65943cf80b8886aea
carla-caracola <carlabiscotti@hotmail.com>
2024-07-05 16:08:10 +0200

    update pair_2 with exercises 2 and 3 solved

--- pair-2-SQL-joins.sql
+++ pair-2-SQL-joins.sql
@@ -10,7 +10,34 @@ SELECT c.customer_id, c.company_name, COUNT(o.order_id) AS order_count
     GROUP BY customer_id, company_name;
     
 -- exercise 2 -- cuántos objetos totales compró cada empresa de UK por año
-SELECT customers.company_name, YEAR (orders.order_date),  
+SELECT customers.company_name, YEAR (orders.order_date) AS orders_year, SUM(order_details.quantity)
+	FROM customers
+    JOIN 
+		orders ON customers.customer_id = orders.customer_id
+	JOIN 
+		order_details ON orders.order_id = order_details.order_id
+	WHERE country = "UK"
+    GROUP BY company_name, orders_year;
+
+-- exercise 3 
+SELECT customers.company_name, YEAR (orders.order_date) AS orders_year, SUM(order_details.quantity), SUM(order_details.quantity * order_details.unit_price * (1 - order_details.discount)) 
+	FROM customers
+    JOIN 
+		orders ON customers.customer_id = orders.customer_id
+	JOIN 
+		order_details ON orders.order_id = order_details.order_id
+	WHERE country = "UK"
+    GROUP BY company_name, orders_year;
+
+
+
+
+    
+        
+        
+		
+		
+	
 
 
 
